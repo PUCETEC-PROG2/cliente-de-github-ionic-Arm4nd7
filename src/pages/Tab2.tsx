27a0111cbf655aeb37942905fef4a52144ef4b9c
Arm4nd7 <armando.estevezt@gmail.com>
2025-12-02 21:07:44 -0500

    agregando componentes en tab 1,2 y tab3

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { IonInput } from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -16,7 +18,11 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className='inputRepo'>
+          <IonInput label="Nombre repositorio" labelPlacement="floating" fill="outline" placeholder="Ingresa nombre repositorio"></IonInput>
+          <IonInput label="Descripcion repositorio" labelPlacement="floating" fill="outline" placeholder="Ingresa nombre repositorio"></IonInput>
+        </div>
+
       </IonContent>
     </IonPage>
   );
