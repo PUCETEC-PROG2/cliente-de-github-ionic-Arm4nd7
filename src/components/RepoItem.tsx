ddc66a613d707bbe370764330de10fd7bbb41fc5
Arm4nd7 <armando.estevezt@gmail.com>
2025-12-12 13:24:18 -0500

    unificando propiedades para un solo archivo, se llama a la interface y se pasa los valores

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,30 +1,29 @@
 import './RepoItem.css'
 import React from 'react';
 import {
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
     IonItem,
     IonLabel,
-    IonList,
     IonThumbnail,
 } from '@ionic/react';
-import { airplane, bluetooth, call, wifi } from 'ionicons/icons';
 
-interface RepoProps {
-    name: string;
-    imageUrl: string;
-}
+import { RepositoryItem } from '../interfaces/RepositoryItem';
 
-const RepoItem: React.FC<RepoProps> = ({ name, imageUrl }) => {
+const RepoItem: React.FC<RepositoryItem> = ({ name, description, imageUrl, owner, language }) => {
     return (
         <IonItem>
             <IonThumbnail slot="start">
-                <img alt="Lista de repos" src={ imageUrl || "https://ionicframework.com/docs/img/demos/thumbnail.svg" }/>
+                <img alt="Lista de repos" src={imageUrl || "https://ionicframework.com/docs/img/demos/thumbnail.svg"} />
             </IonThumbnail>
-            <IonLabel>{name}</IonLabel>
+            <IonLabel>
+                <h3>{name}</h3>
+                <p>Propietario: {owner}</p>
+                <div>
+                    <br />
+                    <p>{description}</p>
+                    <br />
+                </div>
+                <p>Lenguaje: {language}</p>
+            </IonLabel>
         </IonItem>
     );
 };
