2ca6a0b8334b156780ee552c4714a43ece466669
Arm4nd7 <armando.estevezt@gmail.com>
2026-01-05 23:13:57 -0500

    configurando peticiones http de la api de github

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -6,7 +6,7 @@ const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
 const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN;
 
 /**
- * 
+ *
  * @returns Obtener repositorios
  */
 
@@ -14,13 +14,13 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
   try {
     const response = await axios.get(`${GITHUB_API_URL}/user/repos`, {
       headers: {
-        Authorization: GITHUB_API_TOKEN
+        Authorization: GITHUB_API_TOKEN,
       },
       params: {
         per_page: 100,
         sort: "created",
         direction: "desc",
-        afiliation: "owner"
+        affiliation: "owner",
       },
     });
     const repositories: RepositoryItem[] = response.data.map((repo: any) => ({
@@ -37,33 +37,113 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
   }
 };
 
+/**
+ * Obtener un repositorio específico
+ */
+export const fetchRepository = async (owner: string, repoName: string): Promise<RepositoryItem | null> => {
+    try {
+      const response = await axios.get(`${GITHUB_API_URL}/repos/${owner}/${repoName}`, {
+        headers: {
+          Authorization: GITHUB_API_TOKEN,
+        },
+      });
+      const repo = response.data;
+      return {
+        name: repo.name,
+        owner: repo.owner ? repo.owner.login : null,
+        description: repo.description ? repo.description : null,
+        imageUrl: repo.owner ? repo.owner.avatar_url : null,
+        language: repo.language ? repo.language : null,
+      };
+    } catch (error) {
+      console.error("Error al obtener el repositorio", error);
+      return null;
+    }
+  };
 
 /**
  * Crear repositorios
- * @param repo 
+ * @param repo
  */
 export const createRepository = async (repo: RepositoryItem): Promise<void> => {
   try {
     const response = await axios.post(`${GITHUB_API_URL}/user/repos`, repo, {
       headers: {
-        Authorization: GITHUB_API_TOKEN
-      }
+        Authorization: GITHUB_API_TOKEN,
+      },
     });
-    console.log("Se creo el repositorio...", response.data)
+    console.log("Se creo el repositorio...", response.data);
   } catch (error) {
-    console.error("Error al crear: ", error)
+    console.error("Error al crear: ", error);
+  }
+};
+
+/**
+ * Editar repositorios
+ * @param repoName 
+ * @param updatedData 
+ */
+export const editRepository = async (
+  owner: string,
+  repoName: string,
+  updatedData: Partial<RepositoryItem>
+): Promise<void> => {
+  try {
+    const response = await axios.patch(
+      `${GITHUB_API_URL}/repos/${owner}/${repoName}`,
+      {
+        name: updatedData.name,
+        description: updatedData.description
+      },
+      {
+        headers: {
+          Authorization: GITHUB_API_TOKEN,
+        },
+      }
+    );
+    console.log("Se edito el repositorio...", response.data);
+  } catch (error) {
+    console.error("Error al editar: ", error);
+    throw error;
   }
 }
 
+/**
+ * Eliminar repositorios
+ * @param repoName 
+ * @param owner 
+ */
 
+export const deleteRepository = async (
+  repoName: string,
+  owner: string
+): Promise<void> => {
+  try {
+    const response = await axios.delete(
+      `${GITHUB_API_URL}/repos/${owner}/${repoName}`,
+      {
+        headers: {
+          Authorization: GITHUB_API_TOKEN,
+        },
+      }
+    );
+    console.log("Se elimino el repositorio...", response.data);
+  } catch (error) {
+    console.error("Error al eliminar: ", error);
+    throw error;
+  }
+};
 
-//vamos a retornar algo para el usuario
+/**
+ *
+ * @returns retornamos algo para el usuairo
+ */
 export const getUserInfo = async (): Promise<UserInfo | null> => {
   try {
     const response = await axios.get(`${GITHUB_API_URL}/user`, {
       headers: {
-        Authorization: GITHUB_API_TOKEN
-      }
+        Authorization: GITHUB_API_TOKEN,
+      },
     });
     return response.data;
   } catch (error) {
@@ -71,4 +151,4 @@ export const getUserInfo = async (): Promise<UserInfo | null> => {
     alert("Error al recuperar los datos del usuario");
     return null;
   }
-}
+};
